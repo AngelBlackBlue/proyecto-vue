d2ca27e64d728f095cab86f741393d8de83472b0
Angel Suarez <112484606+AngelBlackBlue@users.noreply.github.com>
2025-08-10 01:10:27 -0300

    update

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import Contador from './components/Contador.vue';
+import Counter from './components/Counter.vue';
 </script>
 
 <template>
 
-  <Contador />
+  <Counter />
 
 </template>
 
