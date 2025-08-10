44c26875ee568850060ee7f0da0f6b9dbbac3615
Angel Suarez <112484606+AngelBlackBlue@users.noreply.github.com>
2025-08-10 01:09:22 -0300

    component counter

---
+++ src/components/Contador.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+const count = ref(0);
+const increment = () => count.value++;
+const decrement = () => count.value--;
+</script>
+
+<template>
+  <h1>Hello</h1>
+<p>{{ count }}</p>
+<button @click="increment">Increment</button>
+<button @click="decrement">Decrement</button>
+</template>
+
+<style scoped>
+p {
+  color: red;
+}
+</style>
